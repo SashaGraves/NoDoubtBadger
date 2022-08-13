31ae52b57dd99863d20389df4199c1ca1a0815f5
sashagraves <all-exa@ya.ru>
2022-08-13 12:40:28 +0200

    mobile pics

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// menu
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -8,6 +9,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
+// button up
+
 const upButton = document.querySelector('button.up');
 upButton.onclick = function (e) {
   e.preventDefault();
@@ -17,42 +20,66 @@ upButton.onclick = function (e) {
   });
 }
 
-  const modal = document.querySelector("div.modal")
-  const modalImage = modal.querySelector("img")
-  const modalButton = modal.querySelector("button")
+// modal
 
-  modalButton.onclick = function() {
+const modal = document.querySelector("div.modal")
+const modalImage = modal.querySelector("img")
+const modalButton = modal.querySelector("button")
+
+modalButton.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modalImage.src = "";
     modal.style.display = "none";
   }
+} 
 
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modalImage.src = "";
-      modal.style.display = "none";
-    }
-  } 
-
+if( navigator.userAgent.match(/Android/i) ||
+	navigator.userAgent.match(/webOS/i) ||
+	navigator.userAgent.match(/iPhone/i) ||
+	navigator.userAgent.match(/iPod/i)) {
+    
+   
+    
+    document.querySelectorAll("button.art").forEach(button => {
+      button.onclick = function (e) {
+        e.preventDefault();
+        const img = button.querySelector('img');
+        const src = img.dataset.srcmob;
+        
+        modalImage.src = src;
+        modal.style.display = "block";
+      }
+    });
+} else {
   document.querySelectorAll("button.art").forEach(button => {
-    button.onclick = function (e) {
-      e.preventDefault();
-      const img = button.querySelector('img');
-      const src = img.dataset.src;
-      
-      modalImage.src = src;
-      modal.style.display = "block";
-    }
-  });
+  button.onclick = function (e) {
+    e.preventDefault();
+    const img = button.querySelector('img');
+    const src = img.dataset.src;
+    
+    modalImage.src = src;
+    modal.style.display = "block";
+  }
+});
+}
 
+
+
+// slider
   
-  const slider = document.querySelector("#slider")
-  const foregroundImg = document.querySelector(".foreground-img")
-  const sliderButton = document.querySelector(".slider-button")
+const slider = document.querySelector("#slider")
+const foregroundImg = document.querySelector(".foreground-img")
+const sliderButton = document.querySelector(".slider-button")
 
-  slider.addEventListener("input", (e)=>{
-    const sliderPos = e.target.value;
-    // Update the width of the foreground image
-    foregroundImg.style.width = `${sliderPos}%`
-    // Update the position of the slider button
-    sliderButton.style.left = `calc(${sliderPos}% - 19px)`
-  });
+slider.addEventListener("input", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  foregroundImg.style.width = `${sliderPos}%`
+  // Update the position of the slider button
+  sliderButton.style.left = `calc(${sliderPos}% - 19px)`
+});
   
